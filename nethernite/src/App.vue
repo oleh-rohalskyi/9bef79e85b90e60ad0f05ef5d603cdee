abbbf6ee538149e441aa2dd6e1866852d7c13f48
Oleh <oceanfull@protonmail.com>
2021-10-28 21:11:55 +0300

    Add files via upload

---
+++ nethernite/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="wrap d-flex flex-column">
+        <v-card shaped class="content mb-3">
+          <router-view />
+        </v-card>
+        <Footer class="pa-3" />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Footer
+  },
+  name: "App",
+  data: () => ({})
+};
+</script>
+
+<style lang="sass" scoped>
+.wrap
+  min-height: 100vh
+  .content
+    flex: 1
+</style>
